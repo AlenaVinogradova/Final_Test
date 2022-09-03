2bbc4472e21f760450f82d11c4ae28527425c49d
Alena Vinogradova <alchesh42@gmail.com>
2022-09-03 20:23:23 +0300

    print array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] aarf = new string[5];
+
+aarf[0] = "wow";
+aarf[1] = "2";
+aarf[2] = "set";
+aarf[3] = "?";
+aarf[4] = "90";
+
+void PrintArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+PrintArray(aarf);
